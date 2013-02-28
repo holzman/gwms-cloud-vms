3e38701487d067c40ffe9ac1c2ac18ea10999850
Anthony Tiradani <tiradani@fnal.gov>
2013-02-28 13:50:31 -0600

    added a module to encapsulate the different runtime contextualization methods

---
+++ pilotservices/glideinwms_pilot/contextualization_types.py
@@ -0,0 +1,15 @@
+"""
+Valid Contextualization Types
+
+    EC2 - encompasses all cloud implementations that use a meta-data server that
+          responds to http://169.254.169.254/latest/user-data to serve the user-
+          data to the VM
+
+    NIMBUS - Nimbus is similar to EC2 in that they use a meta-data server to 
+          serve the user-data, but they require an extra step.  You must look in
+          the /var/nimbus-metadata-server-url file to determine the address of 
+          the meta-data server.
+
+""" 
+CONTEXT_TYPE_EC2 = "EC2"
+CONTEXT_TYPE_NIMBUS = "NIMBUS"
