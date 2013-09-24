7e3e16ece391663100760a2ae3854af8c4a38658
Anthony Tiradani <tiradani@fnal.gov>
2013-09-24 10:58:40 -0500

    Revert "refactored the different user data types into classes"
    This reverts commit 12ba6b565e83c1dc79abdc5f336ef44ac9939290.

--- pilotservices/glideinwms_pilot/user_data.py
+++ pilotservices/glideinwms_pilot/user_data.py
@@ -23,26 +23,116 @@ def smart_bool(s):
     s = str(s).strip().lower()
     return not s in ['false', 'f', 'n', '0', '']
 
-def GlideinWMSUserData(config):
-    user_data = None
-    context_type = config.contextualization_type.upper()
-    if context_type == CONTEXT_TYPE_EC2:
-        user_data = EC2UserData(config)
-    elif context_type == CONTEXT_TYPE_NIMBUS:
-        user_data = NimbusUserData(config)
-    elif context_type == CONTEXT_TYPE_OPENNEBULA:
-        user_data = OneUserData(config)
 
-    return user_data
+def opennebula_context_disk():
+    context_disk = "/dev/sr0"
+    distname_pattern = [
+        'red hat*',
+        'redhat*',
+        'scientific linux*',
+        'centos*',
+    ]
+
+    version_map = {
+        '5': '/dev/hdc',
+        '6': '/dev/sr0',
+        '7': '/dev/sr0',
+    }
+
+    try:
+        distro = platform.linux_distribution()
+    except:
+        distro = platform.dist()
+
+    regex = "(" + ")|(".join(distname_pattern) + ")"
+    if re.match(regex, distro[0].lower()):
+        context_disk = version_map.get(distro[1][0], context_disk)
+
+    return context_disk
+
+
+def one_ec2_user_data(contextfile):
+    ec2_user_data = ''
+    fd = None
+    try:
+        fd = open(contextfile, 'r')
+        for line in fd.readlines():
+            if line.startswith('EC2_USER_DATA='):
+                ec2_user_data = line[(line.find('=')+1):].strip()
+                break
+    finally:
+        if fd:
+            fd.close()
+
+    return ec2_user_data
+
 
 class UserData(object):
     def __init__(self, config):
         self.config = config
-        self.retrieve()
-        self.template = "GlideinWMSUserData :: %s"
+        self.config.log.log_info('Created UserData object')
 
     def retrieve(self):
-        raise("This method must be overridden")
+        context_type = self.config.contextualization_type.upper()
+        self.config.log.log_info('Retrieving ec2_user_data for %s' % context_type)
+        if context_type == CONTEXT_TYPE_EC2:
+            self.ec2_retrieve_user_data()
+        elif context_type == CONTEXT_TYPE_NIMBUS:
+            self.nimbus_retrieve_user_data()
+        elif context_type == CONTEXT_TYPE_OPENNEBULA:
+            self.one_retrieve_user_data()
+
+
+    def ec2_retrieve_user_data(self):
+        try:
+            user_data_url = self.config.ec2_url
+            # touch the file so that it exists with proper permissions
+            vm_utils.touch(self.config.userdata_file, mode=0600)
+            # Now retrieve userdata into the file
+            self.config.userdata_file, _ = urllib.urlretrieve(user_data_url, self.config.userdata_file)
+        except Exception, ex:
+            raise UserDataError("Error retrieving User Data(context type: EC2): %s\n" % str(ex))
+
+    def one_retrieve_user_data(self):
+        try:
+            # Mount cdrom drive... OpenNebula contextualization unmounts it
+            self.config.log.log_info('Mounting opennebula context disk %s' % opennebula_context_disk())
+            mount_cmd = ["mount", "-t", "iso9660", opennebula_context_disk(), "/mnt"]
+            subprocess.call(mount_cmd)
+
+            # copy the OpenNebula userdata file
+            self.config.log.log_info('Reading context file: %s' % self.config.one_user_data_file)
+            vm_utils.touch(self.config.userdata_file, mode=0600)
+            fd = open(self.config.userdata_file, 'w')
+            user_data = base64.b64decode(one_ec2_user_data(self.config.one_user_data_file))
+            self.config.log.log_info('Writing User data')
+            # Only write to logs for debugging purposes.
+            # Writing ec2_user_data to logs is a security issue.
+            #self.config.log.log_info('Writing User data %s' % user_data)
+            fd.write(user_data)
+            fd.close()
+            umount_cmd = ["umount", "/mnt"]
+            subprocess.call(umount_cmd)
+        except Exception, ex:
+            raise UserDataError("Error retrieving User Data (context type: OPENNEBULA): %s\n" % str(ex))
+
+    def nimbus_retrieve_user_data(self):
+        try:
+            url_file = open(self.config.nimbus_url_file, 'r')
+            url = url_file.read().strip()
+            user_data_url = "%s/2007-01-19/user-data" % url
+            self.config.userdata_file, _ = urllib.urlretrieve(user_data_url, self.config.userdata_file)
+        except IOError, ex:
+            raise UserDataError("Could not open Nimbus meta-data url file (context type: NIMBUS): %s\n" % str(ex))
+        except Exception, ex:
+            raise UserDataError("Error retrieving User Data (context type: NIMBUS): %s\n" % str(ex))
+
+
+class GlideinWMSUserData(UserData):
+    def __init__(self, config):
+        super(GlideinWMSUserData, self).__init__(config)
+        self.retrieve()
+        self.template = "GlideinWMSUserData :: %s"
 
     def extract_user_data(self):
         """
@@ -171,105 +261,3 @@ class UserData(object):
         except Exception, ex:
             raise PilotError("Error extracting User Data: %s\n" % str(ex))
 
-class OneUserData(UserData):
-    def __init__(self, config):
-        super(OneUserData, self).__init__(config)
-        self.config.log.log_info('Created One UserData object')
-
-    def retrieve(self):
-        self.config.log.log_info('Retrieving One user data')
-        try:
-            # Mount cdrom drive... OpenNebula contextualization unmounts it
-            self.config.log.log_info('Mounting opennebula context disk %s' % self.opennebula_context_disk())
-            mount_cmd = ["mount", "-t", "iso9660", self.opennebula_context_disk(), "/mnt"]
-            subprocess.call(mount_cmd)
-
-            # copy the OpenNebula userdata file
-            self.config.log.log_info('Reading context file: %s' % self.config.one_user_data_file)
-            vm_utils.touch(self.config.userdata_file, mode=0600)
-            fd = open(self.config.userdata_file, 'w')
-            user_data = base64.b64decode(self.one_ec2_user_data(self.config.one_user_data_file))
-            self.config.log.log_info('Writing User data')
-            # Only write to logs for debugging purposes.
-            # Writing ec2_user_data to logs is a security issue.
-            #self.config.log.log_info('Writing User data %s' % user_data)
-            fd.write(user_data)
-            fd.close()
-            umount_cmd = ["umount", "/mnt"]
-            subprocess.call(umount_cmd)
-        except Exception, ex:
-            raise UserDataError("Error retrieving User Data (context type: OPENNEBULA): %s\n" % str(ex))
-
-    def opennebula_context_disk(self):
-        context_disk = "/dev/sr0"
-        distname_pattern = [
-            'red hat*',
-            'redhat*',
-            'scientific linux*',
-            'centos*',
-        ]
-
-        version_map = {
-            '5': '/dev/hdc',
-            '6': '/dev/sr0',
-            '7': '/dev/sr0',
-        }
-
-        try:
-            distro = platform.linux_distribution()
-        except:
-            distro = platform.dist()
-
-        regex = "(" + ")|(".join(distname_pattern) + ")"
-        if re.match(regex, distro[0].lower()):
-            context_disk = version_map.get(distro[1][0], context_disk)
-
-        return context_disk
-
-    def one_ec2_user_data(self, contextfile):
-        ec2_user_data = ''
-        fd = None
-        try:
-            fd = open(contextfile, 'r')
-            for line in fd.readlines():
-                if line.startswith('EC2_USER_DATA='):
-                    ec2_user_data = line[(line.find('=')+1):].strip()
-                    break
-        finally:
-            if fd:
-                fd.close()
-
-        return ec2_user_data
-
-class NimbusUserData(UserData):
-    def __init__(self, config):
-        super(NimbusUserData, self).__init__(config)
-        self.config.log.log_info('Created Nimbus UserData object')
-
-    def retrieve(self):
-        self.config.log.log_info('Retrieving Nimbus user data')
-        try:
-            url_file = open(self.config.nimbus_url_file, 'r')
-            url = url_file.read().strip()
-            user_data_url = "%s/2007-01-19/user-data" % url
-            self.config.userdata_file, _ = urllib.urlretrieve(user_data_url, self.config.userdata_file)
-        except IOError, ex:
-            raise UserDataError("Could not open Nimbus meta-data url file (context type: NIMBUS): %s\n" % str(ex))
-        except Exception, ex:
-            raise UserDataError("Error retrieving User Data (context type: NIMBUS): %s\n" % str(ex))
-
-class EC2UserData(UserData):
-    def __init__(self, config):
-        super(EC2UserData, self).__init__(config)
-        self.config.log.log_info('Created EC2 UserData object')
-
-    def retrieve(self):
-        self.config.log.log_info('Retrieving EC2 user data')
-        try:
-            user_data_url = self.config.ec2_url
-            # touch the file so that it exists with proper permissions
-            vm_utils.touch(self.config.userdata_file, mode=0600)
-            # Now retrieve userdata into the file
-            self.config.userdata_file, _ = urllib.urlretrieve(user_data_url, self.config.userdata_file)
-        except Exception, ex:
-            raise UserDataError("Error retrieving User Data(context type: EC2): %s\n" % str(ex))
