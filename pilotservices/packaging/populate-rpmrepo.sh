ebe8f9ce78b079b6e5de741d30d05d8c0a83dff0
Parag Mhashilkar <parag@fnal.gov>
2014-07-31 15:20:07 -0500

    Script to populate rpms to gwmscloud repo

---
+++ pilotservices/packaging/populate-rpmrepo.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+usage="USAGE: $0 <path to rpm file> [prod]"
+remote_cmd() {
+    cmd=$1
+    ssh $repologin "$cmd"
+}
+
+#rpmfile=$1
+repo=$1
+
+#if [ "$rpmfile" = "" ]; then
+#    echo "Specify rpm file"
+#    echo $usage
+#    exit 1
+#fi
+
+# Check with Parag or Tony to get access to the repo
+repologin="gwmscloud@web1.fnal.gov"
+repodir='/var/www/html/files/gwmscloud'
+
+# Root directory where mock puts the files
+mockdir="/var/lib/mock"
+
+#For now only SL6 support
+versionlist='5 6'
+
+#For now only 64bit support
+archlist='x86_64'
+
+# Create repo for dev (developers) and one for production (operations)
+flavors='dev'
+
+#make changes where appropiate (eg: scp new rpms)
+for flavor in $flavors; do
+    for version in $versionlist; do
+        for arch in $archlist; do
+            workdir="$repodir/$flavor/$version/$arch"
+            sourcedir="$mockdir/epel-$version-$arch/result"
+            remote_cmd "mkdir -p $workdir"
+            scp $sourcedir/*rpm "$repologin:$workdir"
+            remote_cmd "createrepo $workdir"
+            #remote_cmd "chmod -R g+w $workdir"
+         done
+    done
+done
